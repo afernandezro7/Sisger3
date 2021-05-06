3c52c1b51e091fceea8d89683c5a9fc897f7a0cc
Alberto Fernandez <a.fernandez.ro7@gmail.com>
2021-05-06 18:15:27 -0500

    resolve envio_filter

--- src/Belraysa/BackendBundle/Repository/EnvioRepository.php
+++ src/Belraysa/BackendBundle/Repository/EnvioRepository.php
@@ -20,5 +20,57 @@ class EnvioRepository extends EntityRepository
         return $query->getResult();
     }
 
+    public function advanceSearch($text)
+    {
+
+
+        $em = $this->getEntityManager();
+
+        $select = 'SELECT e FROM BackendBundle:Envio e ';
+       
+
+        if(strpos($text, 'Contenedor # ') !== false){
+            $join = 'JOIN e.contenedor c ';
+            $where = 'WHERE c.indiceAnual = :query ';
+
+        }else{
+
+            $text1 = '%' . $text . '%';
+            $text2 = $text . '%';
+            $text3 = '%' . $text;
+
+            $join = 'JOIN e.consignado c ';
+            $where = 'WHERE e.sisgerCode LIKE :query1 OR '.  
+                     'e.sisgerCode LIKE :query2 OR '. 
+                     'e.sisgerCode LIKE :query3 OR '. 
+                     'e.remitenteNombre LIKE :query1 OR '. 
+                     'e.remitenteNombre LIKE :query2 OR '. 
+                     'e.remitenteNombre LIKE :query3 OR '. 
+                     'c.firstName LIKE :query1 OR '. 
+                     'c.firstName LIKE :query2 OR '. 
+                     'c.firstName LIKE :query3 OR '. 
+                     'c.lastName LIKE :query1 OR '. 
+                     'c.lastName LIKE :query2 OR '. 
+                     'c.lastName LIKE :query3 '
+            ;
+
+        }
+
+        $sql = $select . $join . $where . 'ORDER BY e.fechaCreacion DESC';
+        $query = $em->createQuery($sql);
+
+        if(strpos($text, 'Contenedor # ') !== false){
+
+            $indiceAnual = explode(" ", $text)[2];
+            $query->setParameter('query', $indiceAnual);
+        }else {
+            $query->setParameter('query1', $text1);
+            $query->setParameter('query2', $text2);
+            $query->setParameter('query3', $text3);
+
+        }
+        return $query->getResult();
+    }
+
 
 }
